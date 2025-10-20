6cd94e326aaad4ea50aebfad1dda3f6629c4a513
memnochdavid <davduquediaz@gmail.com>
2025-10-20 13:03:48 +0200

    htikryidyik

--- ejercicios.php
+++ ejercicios.php
@@ -1,39 +1,19 @@
 <?php
 
-function botonEj($ej){
-    echo "<button type='button'>Ejecuta</button><br> <br>";
-    return match (true){
-        $ej === 1 => return ej01_maxFromArray(),
-}
-
-function execSeminario()
-{
-    $maxEjercicios = 29;
-    echo "DWES - Seminario 1 <br>";
-    echo "EJERCICIOS: <br> <br>";
-    echo "----------------------------------------------- <br> <br>";
-
-    for($i = 1; $i <= $maxEjercicios; $i++){
-        echo "Ejercicio $i: ";
-        botonEj($i);
-    }
-
-}
-
 /*
  * Ejercicio 1. Número máximo de un array
 Crea una función que obtenga el número máximo de un array de números
  * */
 
-$arrayEj1 = [2, 5, 99, 0, 1];
 
-function ej01_maxFromArray(array $arrayEj1)
+use Dom\Element;
+
+function ej01(array $array)
 {
-    echo "El array es: ".var_dump($arrayEj1);
-    $maxVal = $arrayEj1[0];
-    for($i = 1; $i < count($arrayEj1); $i++){
-        if($maxVal < $arrayEj1[$i]){
-            $maxVal = $arrayEj1[$i];
+    $maxVal = $array[0];
+    for($i = 1; $i < count($array); $i++){
+        if($maxVal < $array[$i]){
+            $maxVal = $array[$i];
         }
     }
     return $maxVal;
@@ -44,11 +24,11 @@ function ej01_maxFromArray(array $arrayEj1)
 Crea una función que obtenga la sumatoria de un array de números.
  * */
 
-function ej02_Sumatoria(array $arrayEj1)
+function ej02(array $array)
 {
     $sum = 0;
-    for($i = 0; $i < count($arrayEj1); $i++){
-        $sum += $arrayEj1[$i];
+    for($i = 0; $i < count($array); $i++){
+        $sum += $array[$i];
     }
     return $sum;
 }
@@ -59,8 +39,8 @@ Crea una función que dada una distancia en millas calcule su correspondiente en
 Nota: 1 milla = 1.60934 kilómetros
  * */
 
-function ej03_MILES_to_KM($km){
-    return $km * 1.60934;
+function ej03($km){
+    return $km / 1.60934;
 }
 
 /*
@@ -70,7 +50,7 @@ Nota: Un palíndromo es una palabra o frase que se lee igual de izquierda a dere
 derecha a izquierda, por ejemplo: "ana", "reconocer", "anilina".
  * */
 
-function ej04_isPalindromo($palabra){
+function ej04($palabra){
     if($palabra === "") return false;
     else{
         return $palabra === strrev($palabra);
@@ -83,7 +63,7 @@ Crea una función que cuente cuántas veces aparece una letra en un texto
  *
  * */
 
-function ej05_CuentaLetras($cadenaTexto, $letra){
+function ej05($cadenaTexto, $letra){
     $ocurrencias = 0;
     for($i = 0; $i < strlen($cadenaTexto); $i++){
         if($cadenaTexto[$i] === $letra){
@@ -94,9 +74,142 @@ function ej05_CuentaLetras($cadenaTexto, $letra){
 }
 
 /*
- *
+ *Ejercicio 6. Contar ocurrencias de una subcadena
+Crea una función que cuente cuántas veces aparece una subcadena en un texto.
  * */
 
+function ej06($texto, $subcadena) {
+    $ocurrencias = 0;
+    $lenTexto = strlen($texto);
+    $lenSub = strlen($subcadena);
+    for ($i = 0; $i <= $lenTexto; $i++) {
+        if (substr($texto, $i, $lenSub) === $subcadena) {
+            $ocurrencias++;
+        }
+    }
+    return $ocurrencias;
+}
 
+/*
+ * Ejercicio 7. Capitalizar palabras
+Crea una función que ponga en mayúscula la primera letra de cada palabra de un texto.
+Ejemplo: "hola mundo" → "Hola Mundo"
+ * */
+function ej07($texto) {
+    $palabras = explode(" ", $texto);
+    $output = "";
+    for($i = 0; $i < count($palabras); $i++){
+        $palabras[$i][0] = strtoupper(substr($palabras[$i], 0, 1));
+        $output .= $palabras[$i].=" ";
+    }
+    return $output;
+}
+/*
+ * Ejercicio 8. Suma de dígitos
+Crea una función que sume los dígitos de un número.
+Ejemplo: sumaDigitos(245) = 2 + 4 + 5 = 11
+ * */
+
+function ej08($num) {
+    $numCifras = 0;
+    while(true){
+        if($num % 10 == 0){
+            $numCifras ++;
+        }
+        else break;
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+function validaInput($mensaje, $tipo) {
+    $input = readline($mensaje);
+
+    switch ($tipo) {
+        case 'int':
+            return filter_var($input, FILTER_VALIDATE_FLOAT) !== false ? $input : null;
+        //se pueden añadir más casas para cada tipo
+        default:
+            return null;
+    }
+}
+function execSeminario() {
+    do{
+        $ejecutaEj = validaInput("Ejercicio a ejecutar: ", "int");
+        if($ejecutaEj != null) break;
+    }while(true);
+
+    $array = [2, 5, 99, 0, 1];
+    switch($ejecutaEj){
+        case 1:
+            echo "Array original: ";
+            print_r($array);
+            echo "El valor máximo es: ".ej01($array);
+            break;
+        case 2:
+            echo "Array original: ";
+            print_r($array);
+            echo "La sumatoria: ".ej02($array);
+            break;
+        case 3:
+            $kms = 100;
+            echo "$kms kilómetros son ".ej03($kms)." millas";
+
+            break;
+        case 4:
+            $palabra1 = "perro";
+            $palabra2 = "reconocer";
+            $output = "Palabra 1: ".$palabra1;
+            if(ej04($palabra1)) {
+                $output.=" es un palíndromo.\n";
+            }
+            else {
+                $output.=" no es un palíndromo.\n";
+            }
+            echo $output;
+            $output = "Palabra 2: ".$palabra2;
+            if(ej04($palabra2)) {
+                $output.=" es un palíndromo.\n";
+            }
+            else {
+                $output.=" no es un palíndromo.\n";
+            }
+            echo $output;
+            break;
+        case 5:
+            $letra = 'n';
+            $texto = "Un texto es una composición de signos codificados en un sistema de escritura que forma una unidad de sentido";
+
+            echo "Letra:\n".$letra."\n";
+            echo "Texto:\n".$texto."\n";
+            echo "Apariciones de $letra: ".ej05($texto,$letra);
+            break;
+        case 6:
+            $subcadena = "de";
+            $texto = "Un texto es una composición de signos codificados en un sistema de escritura que forma una unidad de sentido";
+
+            echo "Letra:\n".$subcadena."\n";
+            echo "Texto:\n".$texto."\n";
+            echo "Apariciones de $subcadena: ".ej06($texto,$subcadena);
+            break;
+        case 7:
+            $subcadena = "hola holita, vecinito.";
+            echo "Antes:\n".$subcadena."\n";
+            echo "Después:\n".ej07($subcadena)."\n";
+            break;
+        default:
+            echo "CAGASTE";
+    }
+}
 
 execSeminario();
+
+?>
