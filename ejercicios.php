867fd83de3defd107dcc835c7df5a30a09b9515f
memnochdavid <davduquediaz@gmail.com>
2025-10-22 12:19:45 +0200

    htikryidyik

--- ejercicios.php
+++ ejercicios.php
@@ -202,8 +202,23 @@ function ej13($emmet){
         $elementos = explode("#", $emmet);
         $id = "$elementos[1]";
     }
+    elseif (!str_contains($emmet, ".")){
+        $etiqueta = $emmet;
+    }
 
-    $output = '<div class="'.$class.'" id="'.$id.'">';
+    $output = '<'.$etiqueta;
+    if(empty($class)){
+        $output .= '></'.$etiqueta.'>';
+    }
+    elseif (!empty($class) && empty($id)){
+        $output .= ' class="'.$class.'"></'.$etiqueta.'>';
+    }
+    elseif (empty($class) && !empty($id)){
+        $output .= ' id="'.$id.'"></'.$etiqueta.'>';
+    }
+    else {
+        $output = '<'.$etiqueta.' class="'.$class.'" id="'.$id.'">';
+    }
 
     return $output;
 }
@@ -239,15 +254,207 @@ Ejemplo: comparar([1, 2, 3], [1, 2, 4]) → [true, true, false]
 function ej15($array1, $array2){
     $verifica = [];
     for($i = 0; $i < count($array1); $i++){
-        if($array1[$i] !== $array2[$i]){
-            $verifica[$i] = false;
+        if($array1[$i] === $array2[$i]){
+            $verifica[$i] = true;
         }
         else{
-//            $verifica[$i]
+            $verifica[$i] = false;
+        }
+    }
+//    var_dump($verifica);
+//    die();
+    return $verifica;
+}
+
+
+/*
+ * Ejercicio 16. Producto de elementos de un array
+Crea una función que calcule el producto de todos los elementos en un array de números.
+Ejemplo: producto([2, 3, 4]) → 24
+ * */
+
+function ej16($array, $multiplica){
+    $res = 0;
+    for($i = 0; $i < count($array); $i++){
+        $res += $array[$i] * $multiplica;
+    }
+    return $res;
+}
+
+/*
+ * Ejercicio 17. Filtrar números pares
+Crea una función que dada un array de números, devuelva un nuevo array con solo los
+números pares.
+Ejemplo: filtrarPares([1, 2, 3, 4, 5, 6]) → [2, 4, 6]
+ * */
+
+function ej17($array): array
+{
+    return array_filter($array, function($num){
+        return $num % 2 === 0;
+    });
+}
+
+/*
+ * Ejercicio 18. Número primo
+Crea una función que determine si un número es primo.
+Nota: Un número primo es un número natural mayor que 1 que solo es divisible por 1 y por
+sí mismo.
+ * */
+
+function ej18($num){
+    $tope = $num / 2;
+    if($num % 2 === 0){
+        return false;
+    }
+    for ($i = 3; $i <= $tope; $i += 2) {
+        if ($num % $i === 0) {
+            return false;
         }
     }
     return true;
 }
+
+/*
+ * Ejercicio 19. Eliminar vocales
+Crea una función que, dada una cadena de texto, elimine todas las vocales de la cadena.
+Ejemplo: eliminarVocales("Hola Mundo") → "Hl Mnd"
+ *
+ * */
+
+function ej19($texto){
+    $arrayAux = str_split($texto);
+    $array = array_filter($arrayAux, function($letra){
+        return !str_contains("aeiouAEIOU", $letra);
+    });
+
+    $output ="";
+    foreach($array as $letra){
+        $output .= $letra;
+    }
+    return $output;
+}
+
+/*
+ *Ejercicio 20. Factorial
+Crea una función que calcule el factorial de un número.Nota: El factorial de un número n (representado como n!) es el producto de todos los
+números enteros positivos desde 1 hasta n. Por ejemplo, 5! = 5 × 4 × 3 × 2 × 1 = 120
+ * */
+
+function ej20($n){
+    $factorial = 1;
+    $output = $n."! = ";
+    for ($i = $n; $i >= 1; $i--) {
+        $factorial *= $i;
+        $output .= $i;
+
+        // Añadir '*' excepto después del 1
+        if ($i > 1) {
+            $output .= " * ";
+        }
+    }
+    $output[strlen($output)-1] = " = ";
+    $output .= $factorial;
+
+    return $output;
+}
+
+/*
+ * Ejercicio 21. Invertir cadena
+Crea una función que invierta una cadena de texto. Por ejemplo, "hola" debería convertirse en
+"aloh".
+ * */
+
+function ej21($texto){
+    $otxet = "";
+    for($i = strlen($texto)-1; $i >= 0; $i--){
+        $otxet .= $texto[$i];
+    }
+    return $otxet;
+}
+
+/*
+ * Ejercicio 22. Número perfecto
+Crea una función que, dado un número, devuelva true si es un número perfecto (la suma de
+sus divisores propios positivos es igual al número), o false en caso contrario.
+Ejemplo: 6 es un número perfecto porque sus divisores propios son 1, 2 y 3, y 1 + 2 + 3 = 6.
+ * */
+function ej22_sacaDivs($n){
+    $divs = [];
+    for($i = 1; $i <= $n/2; $i++){
+        if($n % $i === 0){
+            $divs[] = $i;
+        }
+    }
+    return $divs;
+}
+
+function ej22($n){
+    $divisores = ej22_sacaDivs($n);
+    $sumatoria = ej02($divisores);
+
+    echo imprimeArryLinea($divisores)."\n";
+    echo "Sumatoria: ".$sumatoria."\n";
+
+    return (int)($n === $sumatoria);
+}
+
+/*
+ * Ejercicio 23. Número Armstrong
+Crea una función que, dado un número entero, devuelva true si es un número Armstrong (un
+número que es igual a la suma de sus propios dígitos elevados a una potencia). Por ejemplo,
+153 es un número Armstrong porque 1³ + 5³ + 3³ = 153.
+ * */
+
+function ej23($n)
+{
+    $digitos = [];
+    $strAux = strval($n);
+    for($i = 0; $i < strlen($strAux); $i++){
+        $digitos[] = (int)$strAux[$i];
+    }
+
+    for ($i = 1; $i < $n; $i++) {//exponentes
+        $suma = 0;
+        foreach($digitos as $digito){//digitos
+            $suma += pow($digito, $i);
+        }
+        if($n === $suma)return true;
+    }
+    return false;
+}
+
+
+/*
+ *Ejercicio 24. Calculadora de descuentos con constantes
+Crea un programa que utilice constantes para definir diferentes tipos de descuentos
+(DESCUENTO_ESTUDIANTE, DESCUENTO_JUBILADO, DESCUENTO_VIP) y una
+función que calcule el precio final de un producto aplicando el descuento correspondiente
+según el tipo de cliente.
+Valores de las constantes: • DESCUENTO_ESTUDIANTE: 15%
+• DESCUENTO_JUBILADO: 20%
+• DESCUENTO_VIP: 25%
+Ejemplo: calcularPrecioFinal(100, "estudiante") → 85
+ * */
+
+class Constantes {
+    const DESCUENTO_ESTUDIANTE = 15;
+    const DESCUENTO_JUBILADO = 20;
+    const DESCUENTO_VIP = 25;
+}
+
+function ej24($precio, $descuento){
+    return match (strtolower($descuento)){
+        "estudiante" => $precio - ($precio/100 * Constantes::DESCUENTO_ESTUDIANTE),
+        "jubilado" => $precio - ($precio/100 * Constantes::DESCUENTO_JUBILADO),
+        "vip" => $precio - ($precio/100 * Constantes::DESCUENTO_VIP),
+    };
+}
+
+/*
+ *
+ * */
+
 
 
 function validaInput($mensaje, $tipo) {
@@ -261,6 +468,17 @@ function validaInput($mensaje, $tipo) {
             return null;
     }
 }
+
+function imprimeArryLinea($array){
+    $output = "[";
+    foreach ($array as $elemento){
+        $output .= (int)$elemento.",";
+    }
+    $output[strlen($output)-1] = "]";
+    return $output;
+//    return $nombre.' ['.implode(", ", $array).']';//por qué si es false o 0, no se imprime????
+}
+
 function execSeminario() {
     do{
         $ejecutaEj = validaInput("Ejercicio a ejecutar: ", "int");
@@ -358,13 +576,86 @@ function execSeminario() {
             }
             break;
         case 13:
-            $emmet = "a";
-            echo "Emmet:\n$emmet\n";
-            echo "HTML:\n".ej13($emmet)."\n";
+            $emmet1 = "a";
+            $emmet2 = "div.coche";
+            $emmet3 = "div.coche#VWPolo";
+            echo "Emmet 1:\n$emmet1\n";
+            echo "HTML 1:\n".ej13($emmet1)."\n";
+            echo "Emmet 2:\n$emmet2\n";
+            echo "HTML 2:\n".ej13($emmet2)."\n";
+            echo "Emmet 3:\n$emmet3\n";
+            echo "HTML 3:\n".ej13($emmet3)."\n";
             break;
         case 14:
             $n = 6;
             echo ej14($n);
+            break;
+        case 15:
+            $array1 = [1,2,3,4,5];
+            $array2 = [1,2,3,4,55];
+
+            echo imprimeArryLinea($array1, "ArrayEj1:")."\n";
+            echo imprimeArryLinea($array2, "ArrayEj2:")."\n";
+            echo imprimeArryLinea(ej15($array1, $array2), "Array de Verificación:")."\n";
+            break;
+            //DUDA
+        case 16:
+            $array1 = [1,2,3,4,5];
+            $multiplica = 4;
+            echo imprimeArryLinea($array1, "Array multiplicado por $multiplica:");
+            echo " => ".ej16($array1, $multiplica);
+            break;
+        case 17:
+            $array1 = [1,2,3,4,5,6,7,8,9];
+            echo imprimeArryLinea($array1, "Array original:");
+            echo "\n".imprimeArryLinea(ej17($array1), "Sólo valores pares:");
+            break;
+        case 18:
+            $num = 17;
+            if(ej18($num)) echo "El número ".$num." es primo.";
+            else echo "El número ".$num." no es primo.";
+            break;
+        case 19:
+            $texto1 = "Hola tronco esto es una texto de prueba.";
+            $texto2 = ej19($texto1);
+
+            echo "Texto original:\n$texto1\n";
+            echo "Texto sin vocales:\n$texto2\n";
+            break;
+        case 20:
+            $num = 5;
+            echo "El factorial de ".$num." es:\n".ej20($num);
+            break;
+        case 21:
+            $texto1 = "Hola tronco esto es una texto de prueba.";
+            $texto2 = ej21($texto1);
+            echo "Texto original:\n$texto1\n";
+            echo "Texto invertido:\n$texto2\n";
+            break;
+        case 22:
+            $num = 6;
+            if(ej22($num)){
+                echo "El número ".$num." es un número perfecto.";
+            }
+            else{
+                echo "El número ".$num." no es un número perfecto.";
+            }
+            break;
+        case 23:
+            $num = 153;
+            if(ej23($num)){
+                echo "El numero".$num." es un número Amstrong.";
+            }
+            else echo "El numero ".$num." no es un número Amstrong.";
+            break;
+        case 24:
+            $precio = 100;
+            echo "El precio del producto es: ".$precio."€";
+            echo "\nAplicando descuentos...\n";
+            echo "Precio para estudiantes: ".ej24($precio, "estudiante")."€\n";
+            echo "Precio para jubilados: ".ej24($precio, "jubilado")."€\n";
+            echo "Precio para VIP: ".ej24($precio, "vip")."€\n";
+
             break;
         default:
             echo "CAGASTE";
